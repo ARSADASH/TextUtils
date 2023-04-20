42638cdb0612cd40b0b028084ca49f238428a4a4
Sadashiv Kadam <archana.sadashiv-kadam@capgemini.com>
2023-04-20 14:48:23 +0530

    My First Commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import "./App.css";
+import React, { useState } from "react";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar.js";
+import About from "./components/About.js";
+import TextForm from "./components/TextForm.js";
+import Alert from "./components/Alert.js";
+import Contact from "./components/Contact";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState("light");
+  /* ---------Alert into website----------- */
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    /* Object created to set alert : */
+    setAlert({
+      msg: message,
+      type: type,
+    });
+
+    /* Auto dismissing the alert after some time : */
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const togglemode = () => {
+    if (mode === "dark") {
+      setMode("light");
+      document.body.style.background =
+        "linear-gradient(45deg, rgb(241, 241, 241) 50%, rgb(158, 232, 255) 50%)";
+      showAlert("Light mode has been enabled", "success");
+      // document.title = "TextUtils - Light Mode";
+    } else {
+      setMode("dark");
+      document.body.style.background =
+        "linear-gradient(45deg, rgb(92, 92, 92) 50%, rgb(27, 27, 27) 50%)";
+      showAlert("Dark mode has been enabled", "success");
+      
+    }
+  };
+
+  return (
+    <>
+     <Router>
+      <Navbar
+        title="TextUtils"
+        about="About Us"
+        mode={mode}
+        togglemode={togglemode}
+      />
+      <Alert alert={alert} />
+      <Switch>
+        <Route exact path="/about">
+          <About mode={mode}/>
+        </Route>
+        <Route exact path="/contact">
+          <Contact mode={mode}/>
+        </Route>
+        <Route exact path="/">
+            <TextForm
+            heading="TextUtils - Word Counter, Character Counter, Remove Extra Space"
+            summaryHead="Summary Of Your Text"
+            mode={mode}
+            showAlert={showAlert}
+            />
+        </Route>
+      </Switch>
+
+      <Footer creatername="Archana Kadam" />
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/module2.mjs
@@ -0,0 +1,7 @@
+const a = "Archana";
+const b = "Aditi";
+const c = "Shreekush";
+const d = "Prashant";
+
+export default a;
+export {b,c,d};
